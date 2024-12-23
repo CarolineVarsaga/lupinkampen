ca466783a504d682f6bf34c98f27a9255e19fb22
CarolineVarsaga <carolinevarsaga@gmail.com>
2024-12-23 14:24:28 +0100

    ✨adds header component

---
+++ client/src/components/Header.tsx
@@ -0,0 +1,20 @@
+import Logo from "../assets/lupine-logo.svg";
+import { FaUser } from "react-icons/fa";
+import { IoIosMenu } from "react-icons/io";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="logo-container">
+        <img src={Logo} alt="Logga Lupinkampen" className="logo" />
+        <h1 className="logo-name">Lupinkampen</h1>
+      </div>
+      <div className="user-menu-container">
+        <FaUser size={32} className="user-icon" />
+        <IoIosMenu size={40} className="menu" />
+      </div>
+    </header>
+  );
+};
+
+export default Header;
