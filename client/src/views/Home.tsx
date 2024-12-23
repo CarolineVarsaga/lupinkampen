0993607c56605743d2c2c0543ef348acd15a5511
CarolineVarsaga <carolinevarsaga@gmail.com>
2024-12-23 14:24:14 +0100

    ✨ adds home component, imports Button and Header

---
+++ client/src/views/Home.tsx
@@ -0,0 +1,13 @@
+import Button from "../components/Button";
+import Header from "../components/Header";
+
+const Home = () => {
+  return (
+    <>
+      <Header />
+      <Button text="Börja här" />
+    </>
+  );
+};
+
+export default Home;
