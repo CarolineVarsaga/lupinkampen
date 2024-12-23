5f8b44ab009539bf979c0f10eee0c1ac64b32e37
CarolineVarsaga <carolinevarsaga@gmail.com>
2024-12-23 15:06:27 +0100

    :sparkles: adds hero section

---
+++ client/src/components/HeroSection.tsx
@@ -0,0 +1,20 @@
+import Button from "./Button";
+
+const HeroSection = () => {
+  return (
+    <>
+      <section className="hero-section">
+        <h2 className="hero-heading">
+          <span className="word-to-uppercase">Nu</span> ska lupinerna bort!
+        </h2>
+        <p className="hero-text">
+          Var med i kampen du också! Registrera antal plockade lupiner. Tjäna
+          poäng och tävla mot andra! Helt kostnadsfritt.
+        </p>
+        <Button text="Börja här" />
+      </section>
+    </>
+  );
+};
+
+export default HeroSection;
