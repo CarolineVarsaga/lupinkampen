ef962f298bf133c5917a77f721b1d8b6beccaf70
CarolineVarsaga <carolinevarsaga@gmail.com>
2024-12-27 16:34:50 +0100

    :sparkles: adds info page component

---
+++ client/src/views/InfoPage.tsx
@@ -0,0 +1,13 @@
+import MainSection from "../components/infopage/MainSection";
+import TopSection from "../components/infopage/TopSection";
+
+const InfoPage = () => {
+  return (
+    <>
+      <TopSection />
+      <MainSection />
+    </>
+  );
+};
+
+export default InfoPage;
