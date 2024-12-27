649095d498aff77ac80931507aac1de501b89fbf
CarolineVarsaga <carolinevarsaga@gmail.com>
2024-12-27 10:24:31 +0100

    :sparkles: adds info section component

---
+++ client/src/components/home/InfoSection.tsx
@@ -0,0 +1,20 @@
+import Button from "../Button";
+
+const InfoSection = () => {
+  return (
+    <section className="info-section">
+      <h3 className="info-heading">Invasiv art</h3>
+      <p className="info-text">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec libero
+        felis, hendrerit ac dui quis, vestibulum dictum urna. Donec viverra
+        magna nec mauris sagittis, et tristique tellus tristique. Interdum et
+        malesuada fames ac ante ipsum primis in faucibus. Suspendisse quis lacus
+        auctor, ullamcorper nisl volutpat, lacinia risus. Sed vulputate nibh
+        pellentesque odio consequat, non cursus ex fringilla.
+      </p>
+      <Button text="Läs mer" className="readmore-button" />
+    </section>
+  );
+};
+
+export default InfoSection;
