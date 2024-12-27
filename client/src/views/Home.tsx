63fd64245d5bc4987823788628cb95525432bda9
CarolineVarsaga <carolinevarsaga@gmail.com>
2024-12-27 12:31:56 +0100

    ♻️ removes header and footer from home component

--- client/src/views/Home.tsx
+++ client/src/views/Home.tsx
@@ -1,4 +1,3 @@
-import Header from "../components/Header";
 import HeroSection from "../components/home/HeroSection";
 import InfoSection from "../components/home/InfoSection";
 import MapSection from "../components/home/MapSection";
@@ -6,7 +5,6 @@ import MapSection from "../components/home/MapSection";
 const Home = () => {
   return (
     <>
-      <Header />
       <HeroSection />
       <InfoSection />
       <MapSection />
