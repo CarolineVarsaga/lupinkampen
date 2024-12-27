c1b8e50753bbfff762e6e688fa8eb1899927117b
CarolineVarsaga <carolinevarsaga@gmail.com>
2024-12-27 12:32:28 +0100

    :sparkles: adds footer component

---
+++ client/src/components/Footer.tsx
@@ -0,0 +1,10 @@
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="footer-container">Footer, logga här</div>
+      <p className="copyright-text">&copy; 2024 Lupinkampen</p>
+    </footer>
+  );
+};
+
+export default Footer;
