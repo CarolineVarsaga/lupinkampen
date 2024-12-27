1338c9809d79811346ac84c251b27e90830b090c
CarolineVarsaga <carolinevarsaga@gmail.com>
2024-12-27 11:23:59 +0100

    :sparkles: adds map section component

---
+++ client/src/components/home/MapSection.tsx
@@ -0,0 +1,29 @@
+import Button from "../Button";
+import Map from "../../assets/sweden.svg";
+import BackgroundPolygon from "../../assets/leading-municipality-container-bg.svg";
+
+const MapSection = () => {
+  return (
+    <section className="map-section">
+      <div className="round-image-container"></div>
+      <img src={BackgroundPolygon} alt="" className="background-polygon" />
+      <div className="leading-municipality-container">
+        <h3>Just nu</h3>
+        <div>
+          <h4>Ledande kommun:</h4>
+          {/* Hämta kommun från databas */}
+          <p>Örnsköldsviks kommun</p>
+        </div>
+        <div>
+          <h4>Antal plockade lupiner:</h4>
+          {/* Hämta antal från databas */}
+          <p>2753 st</p>
+        </div>
+      </div>
+      <img src={Map} alt="Sverigekarta" className="map" />
+      <Button text="Topplista" className="highscore-button" />
+    </section>
+  );
+};
+
+export default MapSection;
