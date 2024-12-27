b48f9bbb7221ff9755f935a20936435a45cca901
CarolineVarsaga <carolinevarsaga@gmail.com>
2024-12-27 14:57:27 +0100

    :lipstick: adds svg wave

---
+++ client/src/components/SvgWave.tsx
@@ -0,0 +1,30 @@
+interface ISvg {
+  className?: string;
+}
+
+const SvgWave = ({ className }: ISvg) => {
+  return (
+    <div>
+      <svg
+        viewBox="0 0 500 150"
+        preserveAspectRatio="none"
+        className={className}
+      >
+        <path
+          d="M-3.38,57.73 C210.50,230.44 361.17,69.58 500.56,148.53 L501.12,153.47 L0.00,150.00 Z"
+          fill="#483E73"
+          stroke="none"
+          className="purple-path"
+        ></path>
+        <path
+          d="M-3.38,57.73 C210.50,230.44 361.17,69.58 500.56,148.53"
+          fill="none"
+          stroke="white"
+          stroke-width="1"
+        ></path>
+      </svg>
+    </div>
+  );
+};
+
+export default SvgWave;
---
+++ client/src/components/SvgWaveBottom.tsx
@@ -0,0 +1,29 @@
+interface ISvg {
+  className?: string;
+}
+
+const SvgWaveBottom = ({ className }: ISvg) => {
+  return (
+    <div>
+      <svg
+        viewBox="0 0 500 150"
+        preserveAspectRatio="none"
+        className={className}
+      >
+        <path
+          d="M-5.64,113.98 C190.18,31.09 452.03,142.61 500.00,49.98 L500.00,0.00 L0.00,0.00 Z"
+          fill="#483E73"
+          stroke="none"
+        ></path>
+        <path
+          d="M-5.64,113.98 C190.18,31.09 452.03,142.61 500.00,49.98"
+          fill="none"
+          stroke="white"
+          stroke-width="1"
+        ></path>
+      </svg>
+    </div>
+  );
+};
+
+export default SvgWaveBottom;
