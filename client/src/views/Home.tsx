2b8c4f09f12127501e03ba4a10858e2ac89b6d43
CarolineVarsaga <carolinevarsaga@gmail.com>
2024-12-27 10:24:52 +0100

    :sparkles: imports infosection

--- client/src/views/Home.tsx
+++ client/src/views/Home.tsx
@@ -1,11 +1,13 @@
 import Header from "../components/Header";
-import HeroSection from "../components/HeroSection";
+import HeroSection from "../components/home/HeroSection";
+import InfoSection from "../components/home/InfoSection";
 
 const Home = () => {
   return (
     <>
       <Header />
       <HeroSection />
+      <InfoSection />
     </>
   );
 };
