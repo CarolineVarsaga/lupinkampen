f1b42cf98f03eb750fb07379057f7b2e7bafad77
CarolineVarsaga <carolinevarsaga@gmail.com>
2025-01-02 16:15:42 +0100

    :sparkles: adds a div around the text and heading

--- client/src/components/home/HeroSection.tsx
+++ client/src/components/home/HeroSection.tsx
@@ -5,13 +5,15 @@ const HeroSection = () => {
   return (
     <>
       <section className="hero-section">
-        <h2 className="hero-heading">
-          <span className="word-to-uppercase">Nu</span> ska lupinerna bort!
-        </h2>
-        <p className="hero-text">
-          Var med i kampen du också! Registrera antal plockade lupiner. Tjäna
-          poäng och tävla mot andra! Helt kostnadsfritt.
-        </p>
+        <div className="hero-texts-container">
+          <h2 className="hero-heading">
+            <span className="word-to-uppercase">Nu</span> ska lupinerna bort!
+          </h2>
+          <p className="hero-text">
+            Var med i kampen du också! Registrera antal plockade lupiner. Tjäna
+            poäng och tävla mot andra! Helt kostnadsfritt.
+          </p>
+        </div>
         <Button text="Börja här" className="begin-button" />
         <SvgWave className="svg-wave-top" />
       </section>
