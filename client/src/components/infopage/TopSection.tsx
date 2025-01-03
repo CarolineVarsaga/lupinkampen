b8dd8b0e1f93fe406d1a2f7962b216def5e0a56a
CarolineVarsaga <carolinevarsaga@gmail.com>
2025-01-03 12:03:02 +0100

    :sparkles: adds link to the back link

--- client/src/components/infopage/TopSection.tsx
+++ client/src/components/infopage/TopSection.tsx
@@ -1,10 +1,13 @@
+import { Link } from "react-router-dom";
 import SvgWave from "../SvgWave";
 
 const TopSection = () => {
   return (
     <>
       <section className="top-section">
-        <a className="link-back">Tillbaka till startsidan</a>
+        <Link to="/" className="link-back">
+          <a className="link-back">Tillbaka till startsidan</a>
+        </Link>
         <h3 className="top-section-heading">Ett hot mot den svenska floran</h3>
         <p className="top-section-text">
           Lupiner är en invasiv art som utgör ett allvarligt hot mot den svenska
