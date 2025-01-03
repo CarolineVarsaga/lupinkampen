ce65a8650c7e139d43d5fb6f4d27468437c333a3
CarolineVarsaga <carolinevarsaga@gmail.com>
2025-01-03 09:46:33 +0100

    ♻️ minor changes

--- client/src/components/infopage/TopSection.tsx
+++ client/src/components/infopage/TopSection.tsx
@@ -2,19 +2,21 @@ import SvgWave from "../SvgWave";
 
 const TopSection = () => {
   return (
-    <section className="top-section">
-      <a className="link-back">Tillbaka till startsidan</a>
-      <h3 className="top-section-heading">Ett hot mot den svenska floran</h3>
-      <p className="top-section-text">
-        Lupiner är en invasiv art som utgör ett allvarligt hot mot den svenska
-        naturen. De tränger undan inhemska växter genom att konkurrera om
-        resurser som näring, vatten och ljus, vilket leder till att
-        biodiversiteten minskar. Lupiner sprider sig snabbt och kan etablera sig
-        i stora områden, vilket gör att de förändrar hela ekosystem och påverkar
-        både växt- och djurliv negativt.
-      </p>
+    <>
+      <section className="top-section">
+        <a className="link-back">Tillbaka till startsidan</a>
+        <h3 className="top-section-heading">Ett hot mot den svenska floran</h3>
+        <p className="top-section-text">
+          Lupiner är en invasiv art som utgör ett allvarligt hot mot den svenska
+          naturen. De tränger undan inhemska växter genom att konkurrera om
+          resurser som näring, vatten och ljus, vilket leder till att
+          biodiversiteten minskar. Lupiner sprider sig snabbt och kan etablera
+          sig i stora områden, vilket gör att de förändrar hela ekosystem och
+          påverkar både växt- och djurliv negativt.
+        </p>
+      </section>
       <SvgWave className="top-section-wave" />
-    </section>
+    </>
   );
 };
 
