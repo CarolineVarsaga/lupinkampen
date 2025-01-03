7f7d46dc64181a481dd86b83eedd550f61e67a8e
CarolineVarsaga <carolinevarsaga@gmail.com>
2025-01-03 12:04:25 +0100

    :sparkles: adds link to the back link

--- client/src/views/LogInRegisterPage.tsx
+++ client/src/views/LogInRegisterPage.tsx
@@ -1,12 +1,18 @@
-import LoginForm from "../components/login-register-page/LogInForm";
+import { Link } from "react-router-dom";
+import FormLogIn from "../components/login-register-page/form/FormLogIn";
 
 const LogInRegisterPage = () => {
   return (
     <section className="login-page">
-      <a className="link-back">Tillbaka till startsidan</a>
+      <Link to="/" className="link-back">
+        <a className="link-back">Tillbaka till startsidan</a>
+      </Link>
       <div className="log-in-form">
-        <LoginForm />
-        <a className="register-link">Inte medlem? Registrera ett konto</a>
+        <h3>Logga in</h3>
+        <FormLogIn />
+        <Link to="/registrera" className="register-link">
+          <a className="register-link">Inte medlem? Registrera ett konto</a>
+        </Link>
       </div>
     </section>
   );
