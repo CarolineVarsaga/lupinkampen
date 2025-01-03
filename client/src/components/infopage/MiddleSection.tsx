9bea600e8a2f7ff504882cab9d78117ce62c397d
CarolineVarsaga <carolinevarsaga@gmail.com>
2025-01-03 12:02:44 +0100

    :sparkles: links button to log in page

--- client/src/components/infopage/MiddleSection.tsx
+++ client/src/components/infopage/MiddleSection.tsx
@@ -1,58 +1,10 @@
+import { Link } from "react-router-dom";
 import Button from "../Button";
 
 const MiddleSection = () => {
   const LupineImage = "/assets/lupines-field-1280.webp";
   return (
     <>
-      {/* <section className="infopage-middle-section">
-        <div className="infopage-middle-section-info">
-          <h3>Ursprung</h3>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
-            libero felis, hendrerit ac dui quis, vestibulum dictum urna. Donec
-            viverra magna nec mauris sagittis, et tristique tellus tristique.
-          </p>
-          <h3>Bekämpning</h3>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
-            libero felis, hendrerit ac dui quis, vestibulum dictum urna. Donec
-            viverra magna nec mauris sagittis, et tristique tellus tristique.
-          </p>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
-            libero felis, hendrerit ac dui quis, vestibulum dictum urna. Donec
-            viverra magna nec mauris sagittis, et tristique tellus tristique.
-          </p>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
-            libero felis, hendrerit ac dui quis, vestibulum dictum urna. Donec
-            viverra magna nec mauris sagittis, et tristique tellus tristique.
-          </p>
-        </div>
-      </section>
-      <div className="image-container">
-        <SvgWaveBottom className="middle-section-middle-wave" />
-        <img
-          src={LupineImage}
-          alt="Ett fält med många lupiner"
-          className="middle-section-lupine-image"
-          width={1200}
-          height={800}
-        />
-      </div>
-      <section className="infopage-middle-lower-section-info">
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec libero
-          felis, hendrerit ac dui quis, vestibulum dictum urna. Donec viverra
-          magna nec mauris sagittis, et tristique tellus tristique.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec libero
-          felis, hendrerit ac dui quis, vestibulum dictum urna. Donec viverra
-          magna nec mauris sagittis, et tristique tellus tristique.
-        </p>
-        <Button text="Börja här" className="infopage-begin-button" />
-      </section> */}
       <section className="infopage-middle-section">
         <div className="infopage-middle-section-info">
           <h3>Ursprung</h3>
@@ -99,7 +51,9 @@ const MiddleSection = () => {
           felis, hendrerit ac dui quis, vestibulum dictum urna. Donec viverra
           magna nec mauris sagittis, et tristique tellus tristique.
         </p>
-        <Button text="Börja här" className="infopage-begin-button" />
+        <Link to="/logga-in">
+          <Button text="Börja här" className="infopage-begin-button" />
+        </Link>
       </section>
     </>
   );
