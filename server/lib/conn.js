381ebb079ba3182b82e2aec79e88494ee056cc55
CarolineVarsaga <carolinevarsaga@gmail.com>
2025-01-07 16:59:09 +0100

    :sparkles: adds connection file

---
+++ server/lib/conn.js
@@ -0,0 +1,17 @@
+const mysql = require('mysql2'); 
+require('dotenv').config();
+
+const password = process.env.CONNECTION_PASSWORD; 
+const username = process.env.CONNECTION_USERNAME; 
+const database = process.env.CONNECTION_DATABASE;
+const port = process.env.CONNECTION_PORT;
+
+connection = mysql.createConnection({
+  host: 'localhost',
+  port: port,
+  user: username,
+  password: password,
+  database: database
+})
+
+module.exports = connection;
