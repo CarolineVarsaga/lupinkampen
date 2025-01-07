e1542de491e2e4bf8f80d737edc13c0fae439b91
CarolineVarsaga <carolinevarsaga@gmail.com>
2025-01-07 12:11:12 +0100

    :sparkles: imports lupin information

--- client/src/components/infopage/MiddleSection.tsx
+++ client/src/components/infopage/MiddleSection.tsx
@@ -1,59 +1,26 @@
 import { Link } from "react-router-dom";
 import Button from "../Button";
+import LupinInformation from "./LupinInformation";
 
 const MiddleSection = () => {
   const LupineImage = "/assets/lupines-field-1280.webp";
   return (
     <>
       <section className="infopage-middle-section">
-        <div className="infopage-middle-section-info">
-          <h3>Ursprung</h3>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
-            libero felis, hendrerit ac dui quis, vestibulum dictum urna. Donec
-            viverra magna nec mauris sagittis, et tristique tellus tristique.
-          </p>
-          <h3>Bekämpning</h3>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
-            libero felis, hendrerit ac dui quis, vestibulum dictum urna. Donec
-            viverra magna nec mauris sagittis, et tristique tellus tristique.
-          </p>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
-            libero felis, hendrerit ac dui quis, vestibulum dictum urna. Donec
-            viverra magna nec mauris sagittis, et tristique tellus tristique.
-          </p>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
-            libero felis, hendrerit ac dui quis, vestibulum dictum urna. Donec
-            viverra magna nec mauris sagittis, et tristique tellus tristique.
-          </p>
-        </div>
+        <LupinInformation />
+
         <div className="image-container">
           <img
             src={LupineImage}
             alt="Ett fält med många lupiner"
             className="middle-section-lupine-image"
-            width={1200}
-            height={800}
           />
+          <div className="button-container">
+            <Link to="/logga-in" className="infopage-begin-button">
+              <Button text="Börja här" className="infopage-begin-button" />
+            </Link>
+          </div>
         </div>
-      </section>
-      <section className="infopage-middle-lower-section-info">
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec libero
-          felis, hendrerit ac dui quis, vestibulum dictum urna. Donec viverra
-          magna nec mauris sagittis, et tristique tellus tristique.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec libero
-          felis, hendrerit ac dui quis, vestibulum dictum urna. Donec viverra
-          magna nec mauris sagittis, et tristique tellus tristique.
-        </p>
-        <Link to="/logga-in" className="infopage-begin-button">
-          <Button text="Börja här" className="infopage-begin-button" />
-        </Link>
       </section>
     </>
   );
