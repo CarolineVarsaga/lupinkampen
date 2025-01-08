a5cf3e826e2a859c6f4acd281bd91837439c8746
CarolineVarsaga <carolinevarsaga@gmail.com>
2025-01-08 09:11:22 +0100

    :sparkles: log in user

---
+++ server/routes/users/loginUser.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const router = express.Router();
+const connection = require("../../lib/conn");
+require("dotenv").config();
+
+router.post("/", (req, res) => {
+  const salt = process.env.SALT_KEY;
+
+  const userName = req.body.userName;
+  const password = req.body.password;
+
+  connection.connect((err) => {
+    if (err) console.log(err, "error");
+
+    const query = "SELECT * FROM users WHERE userName = ?";
+    const values = [userName];
+
+    connection.query(query, values, (err, data) => {
+      if (err) console.log(err, "error");
+
+      if (data.length > 0) {
+        console.log("user", data[0]);
+        const decryptedPassword = CryptoJS.AES.decrypt(
+          data[0].password,
+          salt
+        ).toString(CryptoJS.enc.Utf8);
+        if (decryptedPassword === password) {
+          res.status(200).json({ user: data[0].userId, message: "logged in" });
+        } else {
+          res.status(401).json({ message: "wrong username or password" });
+        }
+      } else {
+        res.status(401).json({ message: "wrong username or password" });
+      }
+    });
+  });
+});
+
+module.exports = router;
