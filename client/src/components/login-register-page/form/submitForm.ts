a17ec1ccd33f8edbda8eaf1b3d150b50534b1cbc
CarolineVarsaga <carolinevarsaga@gmail.com>
2025-01-08 11:41:13 +0100

    :sparkles: adds submit form function

---
+++ client/src/components/login-register-page/form/submitForm.ts
@@ -0,0 +1,32 @@
+import axios from "axios";
+import { IForm } from "../../../models/formModels";
+
+export const submitForm = async ({
+  formData,
+  selectedOption,
+  setFormData,
+  setSelectedOption,
+  setErrorMessage,
+}: IForm) => {
+  try {
+    const response = await axios.post("http://localhost:3000/users/create", {
+      userName: formData.username,
+      email: formData.email,
+      password: formData.password,
+      userMunicipality: selectedOption,
+    });
+
+    console.log("User created successfully:", response.data);
+    setFormData({
+      username: "",
+      password: "",
+      email: "",
+      confirmpassword: "",
+      municipality: "",
+    });
+    setSelectedOption("");
+  } catch (error) {
+    console.error("Error creating user:", error);
+    setErrorMessage("Det gick inte att skapa användaren. Försök igen.");
+  }
+};
