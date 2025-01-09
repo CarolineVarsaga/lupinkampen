c85e9b093be3282a83d13a92a8ac967dfd5f5305
CarolineVarsaga <carolinevarsaga@gmail.com>
2025-01-09 14:39:22 +0100

    :sparkles: imports verify token

--- server/routes/users/getUser.js
+++ server/routes/users/getUser.js
@@ -1,27 +1,7 @@
 const express = require("express");
-const jwt = require("jsonwebtoken");
 const router = express.Router();
 const connection = require("../../lib/conn");
-
-const verifyToken = (req, res, next) => {
-  const token = req.headers["authorization"];
-  if (!token) {
-    return res.status(403).json({ message: "Access denied. No token provided." });
-  }
-
-  const tokenWithoutBearer = token.split(" ")[1];
-  if (!tokenWithoutBearer) {
-    return res.status(403).json({ message: "Access denied. Invalid token format." });
-  }
-
-  jwt.verify(tokenWithoutBearer, process.env.JWT_SECRET_KEY, (err, decoded) => {
-    if (err) {
-      return res.status(401).json({ message: "Invalid token." });
-    }
-    req.user = decoded;
-    next();
-  });
-};
+const { verifyToken } = require("../../middleware/authMiddleware.js");
 
 router.get("/:userId", verifyToken, (req, res) => {
   console.log("Inside /users/:userId route");
