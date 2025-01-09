d95dee78f6cea1fd7578af6bc8993af5573b619f
CarolineVarsaga <carolinevarsaga@gmail.com>
2025-01-09 15:55:26 +0100

    :sparkles: register lupins, increase and decrease buttons

--- client/src/views/RegisterLupinesPage.tsx
+++ client/src/views/RegisterLupinesPage.tsx
@@ -4,8 +4,25 @@ import axios from "axios";
 
 const RegisterLupinesPage = () => {
   const [lupinsPicked, setLupinsPicked] = useState<number>(0);
+  const [selectedLupins, setSelectedLupins] =
+    useState<string>("Plockade lupiner");
   const userId = localStorage.getItem("userId");
   const token = localStorage.getItem("token");
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    let value = Number(e.target.value);
+    const max = 9999;
+    const min = 1;
+
+    if (value > max) {
+      value = max;
+    }
+    if (value < min) {
+      value = min;
+    }
+
+    setLupinsPicked(value);
+  };
 
   const handleRegisterLupins = async () => {
     try {
@@ -26,58 +43,68 @@ const RegisterLupinesPage = () => {
     }
   };
 
+  const handleIncrease = (lupin: string) => {
+    setSelectedLupins(lupin);
+    setLupinsPicked((prev) => prev + 1);
+  };
+
+  const handleDecrease = (lupin: string) => {
+    setSelectedLupins(lupin);
+    setLupinsPicked((prev) => (prev > 0 ? prev - 1 : 0));
+  };
+
   return (
     <section className="register-lupines">
       <div className="register-lupines-container">
         <div className="register-lupines-container-top">
           <h3>Registrera lupiner</h3>
-          <div className="register-lupines-add-container">
-            <p>Full säck 125L</p>
-            <p>- 1 +</p>
-          </div>
-          <div className="register-lupines-add-container">
-            <p>Halv säck 125L</p>
-            <p>- 0 +</p>
-          </div>
-          <div className="register-lupines-add-container">
-            <p>Full säck 160L</p>
-            <p>- 0 +</p>
-          </div>
-          <div className="register-lupines-add-container">
-            <p>Halv säck 160L</p>
-            <p>- 0 +</p>
-          </div>
-          <div className="register-lupines-add-container">
-            <p>Full säck 240L</p>
-            <p>- 0 +</p>
-          </div>
-          <div className="register-lupines-add-container">
-            <p>Halv säck 240L</p>
-            <p>- 0 +</p>
-          </div>
+
           <div className="register-lupines-add-container">
             <p>Plockade lupiner</p>
-            <input
-              type="number"
-              value={lupinsPicked}
-              onChange={(e) => setLupinsPicked(Number(e.target.value))}
-              min="1"
-            />
-            <button onClick={handleRegisterLupins}>Registrera</button>
+            <div className="lupins-picker-container">
+              <button
+                onClick={() => handleDecrease("plockade lupiner")}
+                className="lupins-button-decrease amount-button"
+              >
+                -
+              </button>
+              <input
+                type="number"
+                value={lupinsPicked}
+                onChange={handleInputChange}
+                min="1"
+                max="2200"
+                className="lupins-input"
+              />
+              <button
+                onClick={() => handleIncrease("plockade lupiner")}
+                className="lupins-button-increase amount-button"
+              >
+                +
+              </button>
+            </div>
           </div>
         </div>
 
         <div className="register-lupines-container-bottom">
           <h3>Antal plockade lupiner</h3>
           <div className="register-lupines-total-container">
-            <p>x1 Full säck 125L</p>
+            {lupinsPicked > 0 && (
+              <p>
+                {lupinsPicked}x {selectedLupins}
+              </p>
+            )}
             <div>
               <hr />
-              <p>Totalt: 25 plockade lupiner</p>
+              <p>Totalt: {lupinsPicked} plockade lupiner</p>
             </div>
           </div>
         </div>
-        <Button text="Spara" className="register-lupines-button-save" />
+        <Button
+          text="Spara"
+          className="register-lupines-button-save"
+          onClick={handleRegisterLupins}
+        />
       </div>
     </section>
   );
