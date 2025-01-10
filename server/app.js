ebd4a735f561f49cf19a589eb98172ff7301dd0e
CarolineVarsaga <carolinevarsaga@gmail.com>
2025-01-10 14:08:48 +0100

    :sparkles: get vercel

--- server/app.js
+++ server/app.js
@@ -21,6 +21,7 @@ app.use("/users", usersRouter);
 // app.use("/associations", associationsRouter); 
 app.use("/municipalities", municipalitiesRouter);
 
+app.get("/", (req, res) => res.send("Express on Vercel"));
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
