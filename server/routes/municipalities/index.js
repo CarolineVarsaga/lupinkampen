9ce4a87d46dcec936035e64fb650f5425fbdef96
CarolineVarsaga <carolinevarsaga@gmail.com>
2025-01-10 10:11:39 +0100

    :sparkles: imports top municipalities route

--- server/routes/municipalities/index.js
+++ server/routes/municipalities/index.js
@@ -2,7 +2,9 @@ const express = require("express");
 const router = express.Router();
 
 const getMunicipalityScore = require("./getMunicipalityScore.js");
+const getLeadingMunicipalities = require("./getLeadingMunicipalities.js");
 
 router.use("/score", getMunicipalityScore); 
+router.use("/topMunicipalities", getLeadingMunicipalities); 
 
 module.exports = router;
