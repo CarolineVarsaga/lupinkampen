f728611799f91f1e689a0bd7ed585b44bd1c4638
CarolineVarsaga <carolinevarsaga@gmail.com>
2025-01-10 10:10:22 +0100

    :sparkles: get user score placement in sweden

---
+++ server/routes/users/getUserScoreSweden.js
@@ -0,0 +1,57 @@
+const express = require("express");
+const router = express.Router();
+const connection = require("../../lib/conn");
+const { verifyToken } = require("../../middleware/authMiddleware.js");
+
+router.get("/:userId", verifyToken, (req, res) => {
+  const { userId } = req.params;
+
+  const userQuery = `
+    SELECT totalPickedLupins
+    FROM users
+    WHERE userId = ?
+  `;
+
+  connection.query(userQuery, [userId], (err, userResult) => {
+    if (err) {
+      console.log("Database query error:", err);
+      return res.status(500).json({ message: "Internal server error" });
+    }
+
+    if (userResult.length === 0) {
+      return res.status(404).json({ message: "Användare hittades inte." });
+    }
+
+    const { totalPickedLupins } = userResult[0];
+
+    const leaderboardQuery = `
+      SELECT userId, totalPickedLupins
+      FROM users
+      ORDER BY totalPickedLupins DESC
+    `;
+
+    connection.query(leaderboardQuery, (err, leaderboardResult) => {
+      if (err) {
+        console.log("Database query error:", err);
+        return res.status(500).json({ message: "Internal server error" });
+      }
+
+      if (leaderboardResult.length === 0) {
+        return res.status(404).json({ message: "Inga användare hittades." });
+      }
+
+      const userPlacement = leaderboardResult.findIndex(user => String(user.userId) === String(userId)) + 1;
+
+      if (userPlacement === 0) {
+        return res.status(404).json({ message: "Användaren finns inte på rankningen. Kontrollera om du har registrerat några plockade lupiner." });
+      }
+
+      res.status(200).json({
+        userPlacement,
+        totalPickedLupins,
+      });
+    });
+  });
+});
+
+module.exports = router;
