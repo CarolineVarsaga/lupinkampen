77fedfcb5198480ba3036de1160af5453c6792a8
CarolineVarsaga <carolinevarsaga@gmail.com>
2025-01-14 13:22:19 +0100

    ⚗️ adds logs

--- client/src/components/login-register-page/form/FormLogIn.tsx
+++ client/src/components/login-register-page/form/FormLogIn.tsx
@@ -29,6 +29,15 @@ const FormLogIn = () => {
     e.preventDefault();
 
     try {
+      console.log(
+        "Making POST request to:",
+        `${import.meta.env.VITE_API_URL}/api/users/login`
+      );
+      console.log("Payload:", {
+        userName: formData.username,
+        password: formData.password,
+      });
+
       const response = await axios.post(
         `${import.meta.env.VITE_API_URL}/api/users/login`,
         {
