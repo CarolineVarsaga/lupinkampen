67907aa70808d03bec0301fef62d1b35f21fa7fd
CarolineVarsaga <carolinevarsaga@gmail.com>
2025-01-16 11:04:45 +0100

    :sparkles: adds lupins service

---
+++ client/src/services/registerLupinService.ts
@@ -0,0 +1,26 @@
+import { postRequest } from "./baseService";
+
+interface IRegisterLupinsData {
+  lupinsPicked: number;
+}
+
+interface IRegisterLupinsResponse {
+  message: string;
+}
+
+export const registerLupins = async (userId: string, lupinsPicked: number) => {
+  const data: IRegisterLupinsData = { lupinsPicked };
+
+  try {
+    const response = await postRequest<
+      IRegisterLupinsData,
+      IRegisterLupinsResponse
+    >(`/api/users/registerLupins/${userId}`, data, true);
+
+    console.log("Registrering lyckades:", response.message);
+    alert("Lupiner registrerade!");
+  } catch (error) {
+    console.error("Fel vid registrering av lupiner:", error);
+    alert("Kunde inte registrera lupiner.");
+  }
+};
