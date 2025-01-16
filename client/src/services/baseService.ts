78892cd9c0346846df8459e089e9b2af511909cd
CarolineVarsaga <carolinevarsaga@gmail.com>
2025-01-16 11:03:23 +0100

    :sparkles: adds base services

---
+++ client/src/services/baseService.ts
@@ -0,0 +1,58 @@
+import axios from "axios";
+import { baseURL } from "../utils/baseUrl";
+
+const token = localStorage.getItem("token");
+
+//=======================================================
+//===================   EXPORTS   =======================
+//=======================================================
+
+export const customAxios = axios.create({
+  baseURL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+//=======================================================
+
+export const postRequest = async <T, U>(
+  url: string,
+  data: T,
+  withToken: boolean = false
+): Promise<U> => {
+  try {
+    const headers = withToken
+      ? {
+          Authorization: `Bearer ${token || ""}`,
+        }
+      : {};
+
+    const response = await customAxios.post(url, data, { headers });
+    return response.data;
+  } catch (error) {
+    console.error(`Error posting data to ${url}:`, error);
+    throw error;
+  }
+};
+
+//=======================================================
+
+export const getRequest = async <T>(
+  url: string,
+  withToken: boolean = false
+): Promise<T> => {
+  try {
+    const headers = withToken
+      ? {
+          Authorization: `Bearer ${token || ""}`,
+        }
+      : {};
+
+    const response = await customAxios.get(url, { headers });
+    return response.data;
+  } catch (error) {
+    console.error(`Error fetching data from ${url}:`, error);
+    throw error;
+  }
+};
