1055630c07a1372e27d2102c1909cee73fb363cb
CarolineVarsaga <carolinevarsaga@gmail.com>
2025-01-17 09:46:09 +0100

    💫 adds framer motion to menu

--- client/src/components/menu/DropDownMenu.tsx
+++ client/src/components/menu/DropDownMenu.tsx
@@ -4,6 +4,7 @@ import Button from "../Button";
 import { Link, useNavigate } from "react-router-dom";
 import { useAuth } from "../../hooks/useAuth";
 import ConfirmationModal from "../ConfirmationModal";
+import { AnimatePresence, motion } from "framer-motion";
 
 const DropDownMenu = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -11,7 +12,7 @@ const DropDownMenu = () => {
   const navigate = useNavigate();
   const { logout, isAuthenticated } = useAuth();
 
-  const toggleDropdown = () => {
+  const toggleMenu = () => {
     setIsOpen(!isOpen);
   };
 
@@ -23,66 +24,89 @@ const DropDownMenu = () => {
     logout();
     navigate("/logga-in");
     setIsModalOpen(false);
+    toggleMenu();
   };
 
   const handleCancelLogout = () => {
     setIsModalOpen(false);
   };
-
+  const menuVariants = {
+    hidden: { x: "100%" },
+    visible: { x: 0 },
+    exit: { x: "100%" },
+  };
   return (
     <div className="dropdown" aria-label="menu">
-      <button onClick={toggleDropdown} className="dropdown-button">
+      <button onClick={toggleMenu} className="dropdown-button">
         <IoIosMenu size={40} className="menu" />
       </button>
-
-      {isOpen && (
-        <>
-          <div className="dropdown-menu">
-            <div className="heading-and-close">
-              <h3 className="menu-heading">Meny</h3>
-              <IoMdClose
-                size={40}
-                onClick={toggleDropdown}
-                className="close-x"
-              />
+      <AnimatePresence>
+        {isOpen && (
+          <>
+            <div className="dropdown-menu-overlay">
+              <div className="dropdown-menu-relative-parent">
+                <motion.div
+                  className="dropdown-menu"
+                  initial="hidden"
+                  animate="visible"
+                  exit="exit"
+                  variants={menuVariants}
+                  transition={{ duration: 0.5, ease: "easeInOut" }}
+                >
+                  <div className="heading-and-close">
+                    <h3 className="menu-heading">Meny</h3>
+                    <IoMdClose
+                      size={40}
+                      onClick={toggleMenu}
+                      className="close-x"
+                    />
+                  </div>
+                  <div className="dropdown-navigation-container">
+                    <ul>
+                      <Link to="/" onClick={toggleMenu}>
+                        <li className="dropdown-item">Startsida</li>
+                      </Link>
+                      <Link to="/information" onClick={toggleMenu}>
+                        <li className="dropdown-item">
+                          Information om lupiner
+                        </li>
+                      </Link>
+                      <Link to="/topplista" onClick={toggleMenu}>
+                        <li className="dropdown-item">Topplista</li>
+                      </Link>
+                      <Link to="/registrera-lupiner" onClick={toggleMenu}>
+                        <li className="dropdown-item">Registrera lupiner</li>
+                      </Link>
+                      <Link to="/om-lupinkampen" onClick={toggleMenu}>
+                        <li className="dropdown-item">Om Lupinkampen</li>
+                      </Link>
+                      <Link to="/faq" onClick={toggleMenu}>
+                        <li className="dropdown-item">FAQ - Frågor och svar</li>
+                      </Link>
+                      <Link to="/anvandningsvillkor" onClick={toggleMenu}>
+                        <li className="dropdown-item">Användarvillkor</li>
+                      </Link>
+                      {isAuthenticated && (
+                        <li
+                          className="dropdown-item log-out"
+                          onClick={handleLogout}
+                        >
+                          Logga ut
+                        </li>
+                      )}
+                    </ul>
+                    <Button
+                      text="Stäng"
+                      className="close-button"
+                      onClick={toggleMenu}
+                    />
+                  </div>
+                </motion.div>
+              </div>
             </div>
-            <ul>
-              <Link to="/">
-                <li className="dropdown-item">Startsida</li>
-              </Link>
-              <Link to="/information">
-                <li className="dropdown-item">Information om lupiner</li>
-              </Link>
-              <Link to="/topplista">
-                <li className="dropdown-item">Topplista</li>
-              </Link>
-              <Link to="/registrera-lupiner">
-                <li className="dropdown-item">Registrera lupiner</li>
-              </Link>
-              <Link to="/om-lupinkampen">
-                <li className="dropdown-item">Om Lupinkampen</li>
-              </Link>
-              <Link to="/faq">
-                <li className="dropdown-item">FAQ - Frågor och svar</li>
-              </Link>
-              <Link to="/anvandningsvillkor">
-                <li className="dropdown-item">Användarvillkor</li>
-              </Link>
-              {isAuthenticated && (
-                <li className="dropdown-item log-out" onClick={handleLogout}>
-                  Logga ut
-                </li>
-              )}
-            </ul>
-            <Button
-              text="Stäng"
-              className="close-button"
-              onClick={toggleDropdown}
-            />
-          </div>
-        </>
-      )}
-
+          </>
+        )}
+      </AnimatePresence>
       {isModalOpen && (
         <ConfirmationModal
           message="Vill du logga ut?"
