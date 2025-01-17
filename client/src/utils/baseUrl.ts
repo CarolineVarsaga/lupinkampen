72d22914a3a82b638740f35b7fb848ea61ec2874
CarolineVarsaga <carolinevarsaga@gmail.com>
2025-01-17 13:55:16 +0100

    :sparkles:

--- client/src/utils/baseUrl.ts
+++ client/src/utils/baseUrl.ts
@@ -1,5 +1,5 @@
 // export const baseURL =
 //   import.meta.env.VITE_APP_VERCEL_URL || "http://localhost:3001";
 
-//export const baseURL = "https://lupinkampen.vercel.app/api";
-export const baseURL = "http://localhost:3002";
+export const baseURL = "https://lupinkampen.vercel.app/api";
+//export const baseURL = "http://localhost:3002";
