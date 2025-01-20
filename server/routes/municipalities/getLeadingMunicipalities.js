6cde1b09faae5d4117248d8ba6afffdd78cb4cb3
CarolineVarsaga <carolinevarsaga@gmail.com>
2025-01-20 09:05:56 +0100

    :fire: removes old code

--- server/routes/municipalities/getLeadingMunicipalities.js
+++ server/routes/municipalities/getLeadingMunicipalities.js
@@ -20,36 +20,3 @@ router.get("/", async (req, res) => {
 });
 
 module.exports = router;
-
-
-// const express = require("express");
-// const router = express.Router();
-// const connection = require("../../lib/conn");
-
-// router.get("/", (req, res) => {
-//   const topMunicipalitiesQuery = `
-//     SELECT m.municipalityId, m.municipalityName, 
-//       COALESCE(m.municipalityTotalPickedLupins, 0) AS municipalityTotalPickedLupins, 
-//       SUM(u.totalPickedLupins) AS totalLupins
-//     FROM municipalities m
-//     LEFT JOIN users u ON u.userMunicipality = m.municipalityId
-//     GROUP BY m.municipalityId, m.municipalityName, m.municipalityTotalPickedLupins
-//     ORDER BY totalLupins DESC
-//     LIMIT 10;
-//   `;
-
-//   connection.query(topMunicipalitiesQuery, (err, results) => {
-//     if (err) {
-//       console.error("Database query error:", err);
-//       return res.status(500).json({ message: "Internal server error" });
-//     }
-
-//     if (results.length === 0) {
-//       return res.status(404).json({ message: "Inga kommuner hittades." });
-//     }
-
-//     res.status(200).json(results);
-//   });
-// });
-
-// module.exports = router;
--- server/routes/users/getTotalLupins.js
+++ server/routes/users/getTotalLupins.js
@@ -21,30 +21,3 @@ router.get('/', async (req, res) => {
 });
 
 module.exports = router;
-
-
-
-
-// const express = require("express");
-// const router = express.Router();
-// const connection = require("../../lib/conn");
-
-// router.get('/', (req, res) => {
-//   const query = `
-//     SELECT COALESCE(SUM(totalPickedLupins), 0) AS totalLupins
-//     FROM users;
-//   `;
-
-//   connection.query(query, (err, result) => {
-//     if (err) {
-//       console.error("Error fetching total lupins:", err);
-//       return res.status(500).json({ message: "Internal server error" });
-//     }
-
-//     const totalLupins = result[0].totalLupins;
-
-//     res.status(200).json({ totalLupins });
-//   });
-// });
-
-// module.exports = router;
