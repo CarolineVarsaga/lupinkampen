30f9512b590302c926405e84e81af071c7b596c6
CarolineVarsaga <carolinevarsaga@gmail.com>
2025-01-20 14:12:03 +0100

    :sparkles: imports lupines value

--- client/src/views/RegisterLupinesPage.tsx
+++ client/src/views/RegisterLupinesPage.tsx
@@ -2,16 +2,7 @@ import { useState } from "react";
 import Button from "../components/Button";
 import { registerLupins } from "../services/registerLupinService";
 import LupinPicker from "../components/register-lupins/LupinPicker";
-
-const lupinesValue = [
-  { id: 1001, name: "Full säck 125L", lupinesAmount: 50 },
-  { id: 1002, name: "Halv säck 125L", lupinesAmount: 25 },
-  { id: 1003, name: "Full säck 160L", lupinesAmount: 65 },
-  { id: 1004, name: "Halv säck 160L", lupinesAmount: 33 },
-  { id: 1005, name: "Full säck 240L", lupinesAmount: 96 },
-  { id: 1006, name: "Halv säck 240L", lupinesAmount: 48 },
-  { id: 1000, name: "Plockade lupiner", lupinesAmount: 1 },
-];
+import { lupinesValue } from "../models/lupinesValue";
 
 const RegisterLupinesPage = () => {
   const lupinesAmountPerOption: { [key: number]: number } = {};
