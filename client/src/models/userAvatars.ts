5103677e70faeb75d80110962a02de6619314df9
CarolineVarsaga <carolinevarsaga@gmail.com>
2025-01-23 19:58:33 +0100

    :sparkles: adds avatars array

---
+++ client/src/models/userAvatars.ts
@@ -0,0 +1,17 @@
+export const userAvatars = [
+  {
+    avatar: "/assets/avatar-sun.png",
+  },
+  {
+    avatar: "/assets/avatar-bee.png",
+  },
+  {
+    avatar: "/assets/avatar-tulip.png",
+  },
+  {
+    avatar: "/assets/avatar-sunflower.png",
+  },
+  {
+    avatar: "/assets/profile-pic.png",
+  },
+];
