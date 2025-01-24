808c659690cf5f13074c2be58cb121f900cdab90
CarolineVarsaga <carolinevarsaga@gmail.com>
2025-01-24 15:15:21 +0100

    :sparkles: adds hook

---
+++ client/src/hooks/useLeaderboardUserInfo.ts
@@ -0,0 +1,39 @@
+import { useState, useEffect } from "react";
+import { fetchTopUsers } from "../services/leaderboardService";
+import { IUser } from "../models/IUser";
+
+const useLeaderboardUserInfo = () => {
+  const [topUsers, setTopUsers] = useState<IUser[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+
+  const getTopUsers = async () => {
+    setLoading(true);
+    setError(null);
+    try {
+      const data: IUser[] = await fetchTopUsers();
+      console.log("Fetched top users from API:", data);
+      setTopUsers(data);
+    } catch (err) {
+      setError("Kunde inte hämta topplistan över användare.");
+      console.error("Error fetching top users:", err);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getTopUsers();
+  }, []);
+
+  return {
+    topUsers,
+    setTopUsers,
+    setError,
+    loading,
+    error,
+    refetch: getTopUsers,
+  };
+};
+
+export default useLeaderboardUserInfo;
