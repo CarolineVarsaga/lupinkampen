ed46901ca8e91a510224b621985dad0fa26cc4fd
CarolineVarsaga <carolinevarsaga@gmail.com>
2025-01-24 15:17:07 +0100

    :sparkles: imports protected link component

--- client/src/components/menu/DropDownMenu.tsx
+++ client/src/components/menu/DropDownMenu.tsx
@@ -7,11 +7,13 @@ import ConfirmationModal from "../ConfirmationModal";
 import { AnimatePresence, motion } from "framer-motion";
 import useLockScroll from "../../hooks/useLockScroll";
 
+import ProtectedLink from "../ProtectedLink";
+
 const DropDownMenu = () => {
   const [isOpen, setIsOpen] = useState(false);
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const { logout, isAuthenticated, userId: loggedInUserId } = useAuth();
   const navigate = useNavigate();
-  const { logout, isAuthenticated } = useAuth();
 
   useLockScroll(isOpen);
 
@@ -33,11 +35,13 @@ const DropDownMenu = () => {
   const handleCancelLogout = () => {
     setIsModalOpen(false);
   };
+
   const menuVariants = {
     hidden: { x: "100%" },
     visible: { x: 0 },
     exit: { x: "100%" },
   };
+
   return (
     <div className="dropdown" aria-label="menu">
       <button onClick={toggleMenu} className="dropdown-button">
@@ -77,12 +81,13 @@ const DropDownMenu = () => {
                       <Link to="/topplista" onClick={toggleMenu}>
                         <li className="dropdown-item">Topplista</li>
                       </Link>
-                      <Link
-                        to="/profil/:userId/registrera-lupiner"
-                        onClick={toggleMenu}
-                      >
-                        <li className="dropdown-item">Registrera lupiner</li>
-                      </Link>
+                      <li className="dropdown-item">
+                        <ProtectedLink
+                          to={`/profil/${loggedInUserId}/registrera-lupiner`}
+                          text="Registrera lupiner"
+                          toggleMenu={toggleMenu}
+                        />
+                      </li>
                       <Link to="/om-lupinkampen" onClick={toggleMenu}>
                         <li className="dropdown-item">Om Lupinkampen</li>
                       </Link>
