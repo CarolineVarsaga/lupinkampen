0e7478cc201f0d8543522155180fb06056d1241c
CarolineVarsaga <carolinevarsaga@gmail.com>
2025-01-29 10:16:55 +0100

    :sparkles: scroll to top

--- client/src/views/Layout.tsx
+++ client/src/views/Layout.tsx
@@ -1,12 +1,14 @@
 import { Outlet } from "react-router-dom";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import ScrollToTop from "../components/ScrollToTop";
 
 const Layout = () => {
   return (
     <>
       <Header />
       <main className="main-content">
+        <ScrollToTop />
         <Outlet></Outlet>
       </main>
       <Footer />
