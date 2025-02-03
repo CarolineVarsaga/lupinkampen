8a6e6430946799b80b9dfc3668fa02a0d12f15b6
CarolineVarsaga <carolinevarsaga@gmail.com>
2025-02-03 08:01:23 +0100

    ✏️

--- server/app.js
+++ server/app.js
@@ -10,7 +10,7 @@ const municipalitiesRouter = require("./routes/municipalities");
 const app = express();
 
 const corsOptions = {
-  origin: ['http://localhost:3000', 'http://localhost:3001', 'http://localhost:5173', 'https://lupinkampen.vercel.app'],
+  origin: ['http://localhost:3000', 'http://localhost:3001', 'http://localhost:5173', 'https://lupinkampen.vercel.app', 'https://www.lupinkampen.se', 'https://lupinkampen.se'],
   methods: 'GET,POST,UPDATE,DELETE',
   allowedHeaders: 'Content-Type,Authorization'
 };
